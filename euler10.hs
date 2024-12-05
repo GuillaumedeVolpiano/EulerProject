67d1880b36f1371978b20b0c76fc314ee0e0ff04
GuillaumedeVolpiano <63950565+GuillaumedeVolpiano@users.noreply.github.com>
2024-12-05 17:54:43 +0100

    Update euler10.hs

--- euler10.hs
+++ euler10.hs
@@ -1,5 +1,5 @@
 primes :: [Int]
-primes = 2 : filter (isPrime primes) [3,5 ..]
+primes = 2 : filter (isPrime primes) (3:[2*k + 1 | k <- [2..], mod k 3 == 0 || mod k 3 == 2 ])
   where
     isPrime (p:ps) n = p * p > n || n `rem` p /= 0 && isPrime ps n
     
